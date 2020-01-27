85f61043599a2188777c23a7e9e09b0a2ad9c692
auqib <auqib.rather9@gmail.com>
2020-01-28 00:47:12 +0530

    initial commit

---
+++ src/app/config/routes/home.route.ts
@@ -0,0 +1,45 @@
+import {Routes} from '@angular/router';
+import { SiteComponent } from '../../site/site.component';
+import { HomeComponent } from '../../site/home/home.component';
+import { LoginComponent } from '../../site/home/login/login.component';
+import { SignupComponent } from '../../site/home/signup/signup.component';
+import { ForgetPasswordComponent } from '../../site/home/forget-password/forget-password.component';
+import { ResetPasswordComponent } from '../../site/home/reset-password/reset-password.component';
+import { AccountVerifyComponent } from '../../site/home/account-verify/account-verify.component';
+
+export const HOME_ROUTES: Routes = [
+	{
+		path: '',
+		component: HomeComponent,
+		children: [
+			{
+				path: '',
+				redirectTo: '/login',
+				pathMatch: 'full'
+			}
+		]
+	},
+	{
+		path: 'signup',
+		component: SignupComponent,
+	},
+	{
+		path: 'login',
+		component: LoginComponent,
+	},
+	{
+		path: 'forget-password',
+		component: ForgetPasswordComponent,
+	},
+	{
+		path: 'reset-password',
+		component: ResetPasswordComponent
+	},
+	{
+		path:'oath/verify',
+		component:	AccountVerifyComponent
+	},
+
+
+]; 
+
